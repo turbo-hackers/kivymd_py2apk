9f5d05520c7211ef21c58c0292c144b15586fc66
TURB0 <96972645+turbo-hackers@users.noreply.github.com>
2023-11-06 18:29:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,27 @@ from kivymd.uix.button import MDRectangleFlatButton
 class TestApp(MDApp):
 
 	def build(self):
+		self.icon = 'icon.png'
 		global name,l
 		screen = Screen()
 		
-		# defining Text field with all the parameters
-		name = MDTextField(text="Enter name",
-					pos_hint={'center_x': 0.4, 'center_y': 0.5},
-					size_hint_x=None, width=100)
-
 		# defining label with all the parameters
-		l = MDLabel(text="HI !",halign="center",
+		l = MDLabel(text="HI !",
+					halign= "center",
 					pos_hint={'center_y': 0.8},
 					theme_text_color="Custom",
 					text_color=(0.5, 0, 0.5, 1),
 					font_style='Subtitle1')
 
+		# defining Text field with all the parameters
+		name = MDTextField(text="Enter name",
+					pos_hint={'center_x': 0.5, 'center_y': 0.5},
+					size_hint_x=None, width=100)
+
 		# defining Button with all the parameters
 		btn = MDRectangleFlatButton(text="Submit",
 					size_hint_x=None,
-					pos_hint={'center_x': 0.55, 'center_y': 0.5},
+					pos_hint={'center_x': 0.5, 'center_y': 0.4},
 					on_release=self.btnfunc)
 		# adding widgets to screen
 		screen.add_widget(name)
